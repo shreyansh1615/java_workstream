c9310bcc766997f345fb0a564a5cac1010eeccd7
sahoosar <saroj.cit@gmail.com>
2024-02-03 19:19:56 +0000

    Initial commit

---
+++ src/main/java/com.core.deploy/MyTest.java
@@ -0,0 +1,12 @@
+package com.core.deploy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyTest {
+        private static final Logger logger = LoggerFactory.getLogger(MyTest.class);
+
+        public static void main(String[] args) {
+            logger.info("Hello SLF4J!");
+            logger.info("Application deployed Successfully, HAPPY K8 CLUSTER DEPLOYMENT");
+        }
+}
