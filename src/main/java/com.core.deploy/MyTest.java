8c159dd149d91bd0364b16dd123944bab4ba40ce
sahoosar <saroj.cit@gmail.com>
2024-02-04 07:46:24 +0000

    update github actions

--- src/main/java/com.core.deploy/MyTest.java
+++ src/main/java/com.core.deploy/MyTest.java
@@ -6,7 +6,7 @@ public class MyTest {
         private static final Logger logger = LoggerFactory.getLogger(MyTest.class);
 
         public static void main(String[] args) {
-            logger.info("Hello SLF4J!");
+            logger.info("Hello SLF4J! Use for logging ");
             logger.info("Application deployed Successfully, HAPPY K8-CLUSTER DEPLOYMENT");
         }
 }
